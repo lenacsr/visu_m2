88244659a46937a0f54107ec0c910eb50865f0a2
louispwz <louisdragnir@gmail.com>
2025-10-09 15:04:17 +0200

    Add files via upload

--- global.R
+++ global.R
@@ -1,43 +1,53 @@
+########################
+#LIBRARIES
+########################
+# UI / Shiny
 library(shiny)
+library(shinyWidgets)
+library(shinycssloaders)
+library(shinyjs)
+library(bslib)
+library(fontawesome)
+library(htmltools)
+
+# Manip
 library(dplyr)
 library(tidyr)
-library(httr)
-library(rvest)
-library(DT)
-library(ggplot2)
 library(tidyverse)
-library(leaflet)
+library(snakecase)
 
+# Visu
+library(ggplot2)
+library(leaflet)
+library(leaflet.extras2)
 library(sf)
 library(rnaturalearth)
-
 library(rnaturalearthdata)
-#pour exporter la carte en png
-library(leaflet.extras2)
-#pour le titre a exporter de la carte
-library(snakecase)
-#pour mettre le titre de la carte sur la carte pour pouvoir le voir quand tu l'exportes
-library(htmltools )
-library(shinyWidgets)
 
-#Por l'esthetique de l'appli
-library(bslib) #theme
-library(fontawesome)
-library(shinycssloaders)  # Pour loading spinners
-library(shinyjs)
+# TablesShiny
+library(DT)
 
-# DataFrame Global
+# WebScrapping
+library(httr)
+library(rvest)
+
+########################
+#IMPORT DU DATA FRAME
+########################
+
 gender <- read.csv("src/GENDER_STAT_CLEAN.csv", sep = ",")
 
-# Fonctions
 
-## FENETRE DES ANNEES 
+
+########################
+#FONCTIONS
+########################
+
+### Fenetre_glissante
 fenetre_annee <- function(df, annee, n = 2) {
   df_filtre  <- df |>
     mutate(year = as.integer(as.character(year))) |> 
     filter(year >= (annee - n), year <= (annee + n))
-  
-
   
   df_final <- df_filtre |>
     mutate(dist = abs(year - annee)) |>
@@ -49,25 +59,25 @@ fenetre_annee <- function(df, annee, n = 2) {
 }
 
 
-### SCRAPING DES DRAPEAUX 
+### Fonction recup drapeau
 get_flag_url <- function(iso2) {
   iso2 <- tolower(iso2)
   cdn_url <- paste0("https://flagcdn.com/w320/", iso2, ".png")
   
-  # Vérifie si l'image existe
   res <- tryCatch(httr::HEAD(cdn_url), error = function(e) NULL)
   if (!is.null(res) && httr::status_code(res) == 200) {
     return(cdn_url)
   }
-  
-  # Si le CDN ne répond pas, renvoie NULL
   return(NULL)
 }
 
 
 
 
-# Sources
+########################
+# PAGES SOURCES
+########################
+
 source("page/page_tableau_comparaison.R")
 source("page/page_recap_pays.R")
 source("page/page_ACP.R")
@@ -79,7 +89,12 @@ source("page/page_carte_threshold.R")
 
 
 
-# Axes
+########################
+# RESSOURCE POUR L'APP
+########################
+
+### Axes
+
 indicateurs_axes <- list(
   "Éducation" = c(
     "Literacy rate (%)",
@@ -103,7 +118,7 @@ indicateurs_axes <- list(
 )
 
 
-# Palette
+### Palettes
 palette_6 <- c("#FFE100","#F58442","#D96C81","#CB3452","#A2BDF4","#BFB74C")
 palette_continue <- colorRampPalette(palette_6)
 
